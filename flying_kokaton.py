87fce6bceb41533bb741d427d5c1fed1b9b0e8a4
KG <c0b230680f@edu.teu.ac.jp>
2024-04-16 18:30:04 +0900

    完成

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -14,25 +14,32 @@ def main():
     koka_img = pg.image.load("fig/3.png") #練習２
     koka_img = pg.transform.flip(koka_img, True, False)
     koka_rect = koka_img.get_rect()
-    koka_rect.center = 300,200
     tmr = 0
+    koka_rect.center = 300,200
+    
     while True:
+        x = 0
+        y = 0
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            koka_rect.move_ip([0,-1])
-        if key_lst[pg.K_DOWN]:
-            koka_rect.move_ip([0,1])
+            y -= 1
+        elif key_lst[pg.K_DOWN]:
+            y += 1
         if key_lst[pg.K_RIGHT]:
-            koka_rect.move_ip([1,0])
-        if key_lst[pg.K_LEFT]:
-            koka_rect.move_ip([-1,0])
+            x += 2
+        else:
+            x -= 1
+        koka_rect.move_ip([x,y])
+        
+         
+
         screen.blit(bg_img, [-tmr, 0]) #練習７
         screen.blit(bg2_img, [-tmr+1600, 0]) #練習７
         screen.blit(bg_img, [-tmr+3200, 0])
-        screen.blit(koka_img,koka_rect)
+        screen.blit(koka_img,koka_rect )
         pg.display.update()
         tmr += 1        
         if tmr % 3200 == 0:
