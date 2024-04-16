71a19344a8e13440a9a5717ed61e412870fb50f4
KG <c0b230680f@edu.teu.ac.jp>
2024-04-16 17:25:20 +0900

    練習8

--- pygame_image.py
+++ pygame_image.py
@@ -13,15 +13,26 @@ def main():
     bg2_img = pg.transform.flip(bg_img, True, False)
     koka_img = pg.image.load("fig/3.png") #練習２
     koka_img = pg.transform.flip(koka_img, True, False)
+    koka_rect = koka_img.get_rect()
+    koka_rect.center = 300,200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [-tmr, 0])
-        screen.blit(bg2_img, [-tmr+1600, 0])
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            koka_rect.move_ip([0,-1])
+        if key_lst[pg.K_DOWN]:
+            koka_rect.move_ip([0,1])
+        if key_lst[pg.K_RIGHT]:
+            koka_rect.move_ip([1,0])
+        if key_lst[pg.K_LEFT]:
+            koka_rect.move_ip([-1,0])
+        screen.blit(bg_img, [-tmr, 0]) #練習７
+        screen.blit(bg2_img, [-tmr+1600, 0]) #練習７
         screen.blit(bg_img, [-tmr+3200, 0])
-        screen.blit(koka_img, [100, 100])
+        screen.blit(koka_img,koka_rect)
         pg.display.update()
         tmr += 1        
         if tmr % 3200 == 0:
